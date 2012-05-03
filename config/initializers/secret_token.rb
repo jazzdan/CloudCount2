e3d17ad97058b74267c87b9e9a12b6dd2b71aab9
Dan Miller <dan@danielsmiller.com>
2012-05-03 18:36:51 -0400

    Initiated Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudCount2::Application.config.secret_token = 'cb75df49ec57153b8a102252d902399920b5b9a8b0fdd9950464c2973f7e30c69c8c819c9fa06547f41cdb41b7c76411ce69430e88263cd4766a17668ccfce8f'
