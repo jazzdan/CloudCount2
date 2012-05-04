5b1a043ec3539dd7ee5447368ccac52da7e62209
Colby Rabideau <crabideau5691@gmail.com>
2012-05-04 15:36:40 -0400

    made audits pretty

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -1,4 +1,5 @@
 class AuditsController < ApplicationController
+
   def index
     @audits = Audit.all
   end
@@ -7,35 +8,4 @@ class AuditsController < ApplicationController
     @audit = Audit.find(params[:id])
   end
 
-  def new
-    @audit = Audit.new
-  end
-
-  def create
-    @audit = Audit.new(params[:audit])
-    if @audit.save
-      redirect_to @audit, :notice => "Successfully created audit."
-    else
-      render :action => 'new'
-    end
-  end
-
-  def edit
-    @audit = Audit.find(params[:id])
-  end
-
-  def update
-    @audit = Audit.find(params[:id])
-    if @audit.update_attributes(params[:audit])
-      redirect_to @audit, :notice  => "Successfully updated audit."
-    else
-      render :action => 'edit'
-    end
-  end
-
-  def destroy
-    @audit = Audit.find(params[:id])
-    @audit.destroy
-    redirect_to audits_url, :notice => "Successfully destroyed audit."
-  end
 end
