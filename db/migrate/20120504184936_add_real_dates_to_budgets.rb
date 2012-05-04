43b4ed406ec71c434c98289d30d699551abb03fa
Dan Miller <dan@danielsmiller.com>
2012-05-04 17:16:40 -0400

    Removed a migration that broke postgres.

--- db/migrate/20120504184936_add_real_dates_to_budgets.rb
+++
@@ -1,6 +0,0 @@
-class AddRealDatesToBudgets < ActiveRecord::Migration
-    def change
-        change_column :budgets, :starts, :datetime
-        change_column :budgets, :ends, :datetime
-    end
-end
